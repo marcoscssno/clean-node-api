b364b6aa23c2faecbd41a6f9170a43080863a583
Marcos Cassiano <marcoscssno@gmail.com>
2023-03-26 23:28:35 -0300

    feat: create create user controller

--- src/core/useCase/createUserController.ts
+++ src/core/useCase/createUserController.ts
@@ -4,7 +4,24 @@ import { CreateUserUseCase } from "./createUser";
 
 export class CreateUserController {
     constructor(
-        private createUserUseCase = CreateUserUseCase
+        private createUserUseCase: CreateUserUseCase
     ) {}
-    async handle(request: Request, response: Response) {}
+    async handle(request: Request, response: Response): Promise<Response> {
+        const { name, email, encryptedPassword } = request.body;
+
+        try {
+            await this.createUserUseCase.execute({
+                name,
+                email,
+                encryptedPassword
+            })
+
+            return response.status(201).send();
+        }
+        catch(error) {
+            return response.status(400).json({
+                message: error.message || 'Unexpected error'
+            });
+        }
+    }
 }
