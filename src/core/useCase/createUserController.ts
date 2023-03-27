234c2f14c8ab6326bca021700718ce1cb1a3c4ca
Marcos Cassiano <marcoscssno@gmail.com>
2023-03-26 23:22:41 -0300

    chore: add express

--- src/core/useCase/createUserController.ts
+++ src/core/useCase/createUserController.ts
@@ -1,5 +1,10 @@
+import { Request, Response } from 'express';
+import { CreateUserUseCase } from "./createUser";
 
 
 export class CreateUserController {
-
+    constructor(
+        private createUserUseCase = CreateUserUseCase
+    ) {}
+    async handle(request: Request, response: Response) {}
 }
