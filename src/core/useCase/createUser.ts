96b014c208beac7ba8bbbed92290c76a850faf35
Marcos Cassiano <marcoscssno@gmail.com>
2023-03-26 21:46:00 -0300

    refactor: create constructor for create user use case

--- src/core/useCase/createUser.ts
+++ src/core/useCase/createUser.ts
@@ -1,6 +1,7 @@
 import { CreateUserRequestDTO } from "./createUserRequestDTO";
 
 export class CreateUserUseCase {
+    constructor() {}
     async execute(data: CreateUserRequestDTO): Promise<CreateUserRequestDTO> {
         return data;
     }
