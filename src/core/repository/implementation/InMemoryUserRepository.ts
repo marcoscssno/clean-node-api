79e002c455cf050435a0b286384760ff51cef6b8
Marcos Cassiano <marcoscssno@gmail.com>
2023-03-26 21:47:55 -0300

    feat: create in memory user repository impl

---
+++ src/core/repository/implementation/InMemoryUserRepository.ts
@@ -0,0 +1,18 @@
+import { User } from "../../entity/User";
+import { UserRepositoryInterface } from "../UserRepositoryInterface";
+
+export class InMemoryUserRepository implements UserRepositoryInterface {
+    private users: User[] = [];
+    constructor() {}
+    async findByEmail(email: string): Promise<User | undefined> {
+        const user = this.users.find(user => user.getEmail() === email);
+        return user;
+    }
+    async save(user: User): Promise<void> {
+        this.users.push(user);
+    }
+
+    async getAllUsers(): Promise<User[] | []> {
+        return this.users;
+    }
+}
