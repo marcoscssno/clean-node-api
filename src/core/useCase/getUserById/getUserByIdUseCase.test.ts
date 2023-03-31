5384a32b50137d02f96374fa3d25ee95e11d2c92
Marcos Cassiano <marcoscssno@gmail.com>
2023-03-31 15:41:07 -0300

    test: ensure id is passed

---
+++ src/core/useCase/getUserById/getUserByIdUseCase.test.ts
@@ -0,0 +1,10 @@
+import { it, expect, describe } from 'vitest';
+import { GetUserByIdUseCase } from './getUserByIdUseCase';
+
+describe('Get User By Id Use Case', () => {
+    it('should ensure an id is passed', () => {
+        const sut = new GetUserByIdUseCase();
+        const id = 'abc123';
+        expect(sut.execute()).rejects.toThrow();
+    })
+});
