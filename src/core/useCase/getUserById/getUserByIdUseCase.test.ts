5b9fb47cb7755252137db234b2868587fe42840a
Marcos Cassiano <marcoscssno@gmail.com>
2023-03-31 15:43:15 -0300

    style: remove unnecessary declaration

--- src/core/useCase/getUserById/getUserByIdUseCase.test.ts
+++ src/core/useCase/getUserById/getUserByIdUseCase.test.ts
@@ -4,7 +4,6 @@ import { GetUserByIdUseCase } from './getUserByIdUseCase';
 describe('Get User By Id Use Case', async () => {
     it('should ensure an id is passed', () => {
         const sut = new GetUserByIdUseCase();
-        const id = 'abc123';
         expect(sut.execute()).rejects.toThrow();
     });
 });
