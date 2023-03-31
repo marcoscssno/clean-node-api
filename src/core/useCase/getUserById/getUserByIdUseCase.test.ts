f3c3d7df059697e4370d7469c63eafd044246642
Marcos Cassiano <marcoscssno@gmail.com>
2023-03-31 16:27:19 -0300

    test: ensure a repository is specified

--- src/core/useCase/getUserById/getUserByIdUseCase.test.ts
+++ src/core/useCase/getUserById/getUserByIdUseCase.test.ts
@@ -1,13 +1,21 @@
 import { it, expect, describe } from 'vitest';
+import { InMemoryUserRepository } from '../../repository/user/implementation/InMemoryUserRepository';
 import { GetUserByIdUseCase } from './getUserByIdUseCase';
 
 describe('Get User By Id Use Case', async () => {
-    it('should ensure an id is passed', () => {
+    it('should throw an error if no repository is specified', async () => {
         const sut = new GetUserByIdUseCase();
+        const someId: string = 'a1b2c3';
+        expect(async () => sut.execute(someId)).rejects.toThrow();
+    });
+    it('should ensure an id is passed', () => {
+        const userRepository = new InMemoryUserRepository();
+        const sut = new GetUserByIdUseCase(userRepository);
         expect(sut.execute()).rejects.toThrow();
     });
     it('should return null if no user is found with provided id', async () => {
-        const sut = new GetUserByIdUseCase();
+        const userRepository = new InMemoryUserRepository();
+        const sut = new GetUserByIdUseCase(userRepository);
         const someId: string = 'a1b2c3';
         expect(sut.execute(someId)).resolves.toBeNull();
     });
