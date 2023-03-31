568796d37e83848cfbf705c1f70abfc896eb34b2
Marcos Cassiano <marcoscssno@gmail.com>
2023-03-31 19:46:02 -0300

    test: ensure it returns an user if found

--- src/core/useCase/getUserById/getUserByIdUseCase.test.ts
+++ src/core/useCase/getUserById/getUserByIdUseCase.test.ts
@@ -1,9 +1,14 @@
 import { it, expect, describe } from 'vitest';
 import { InMemoryUserRepository } from '../../repository/user/implementation/InMemoryUserRepository';
 import { GetUserByIdUseCase } from './getUserByIdUseCase';
+import { CreateUserUseCase } from '../createUser/createUserUseCase';
+import { BcryptPasswordEncryptor } from '../../lib/passwordEncryptor/BcryptPasswordEncryptor'
+import { CreateUserRequestDTO } from '../createUser/createUserRequestDTO';
+import { GetAllUsersUseCase } from '../getAllUsers/getAllUsersUseCase';
 
 describe('Get User By Id Use Case', async () => {
     it('should throw an error if no repository is specified', async () => {
+        // @ts-expect-error
         const sut = new GetUserByIdUseCase();
         const someId: string = 'a1b2c3';
         expect(async () => sut.execute(someId)).rejects.toThrow();
@@ -11,6 +16,7 @@ describe('Get User By Id Use Case', async () => {
     it('should ensure an id is passed', () => {
         const userRepository = new InMemoryUserRepository();
         const sut = new GetUserByIdUseCase(userRepository);
+        // @ts-expect-error
         expect(sut.execute()).rejects.toThrow();
     });
     it('should return null if no user is found with provided id', async () => {
@@ -19,4 +25,24 @@ describe('Get User By Id Use Case', async () => {
         const someId: string = 'a1b2c3';
         expect(sut.execute(someId)).resolves.toBeNull();
     });
+    it('should return an user if found', async () => {
+        const userRepository = new InMemoryUserRepository();
+        const passwordEncryptor = new BcryptPasswordEncryptor();
+        const createUserUseCase = new CreateUserUseCase(userRepository, passwordEncryptor);
+        const user: CreateUserRequestDTO = {
+            name: 'Marcos',
+            email: 'marcos@example.com',
+            password: 'somePassword'
+        }
+        await createUserUseCase.execute(user);
+        const getAllUsersUseCase = new GetAllUsersUseCase(userRepository);
+        const users = await getAllUsersUseCase.execute();
+        const someId = users[0].getId();
+        console.log('someId ' + someId);
+        const sut = new GetUserByIdUseCase(userRepository);
+        const foundUser = await sut.execute(someId);
+        console.log(foundUser);
+        const { name, email } = user;
+        expect(foundUser).toContain({ name, email });
+    })
 });
