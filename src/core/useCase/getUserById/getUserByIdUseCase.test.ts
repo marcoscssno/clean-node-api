83cca2152dc8592f73086542f4da75e86bebe4e8
Marcos Cassiano <marcoscssno@gmail.com>
2023-03-31 15:47:34 -0300

    test: ensure it returns null if no user is found

--- src/core/useCase/getUserById/getUserByIdUseCase.test.ts
+++ src/core/useCase/getUserById/getUserByIdUseCase.test.ts
@@ -6,4 +6,9 @@ describe('Get User By Id Use Case', async () => {
         const sut = new GetUserByIdUseCase();
         expect(sut.execute()).rejects.toThrow();
     });
+    it('should return null if no user is found with provided id', async () => {
+        const sut = new GetUserByIdUseCase();
+        const someId: string = 'a1b2c3';
+        expect(sut.execute(someId)).resolves.toBeNull();
+    });
 });
