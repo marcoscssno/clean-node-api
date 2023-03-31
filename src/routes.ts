b23d181c104a17768712d093b4826d2382e60c4b
Marcos Cassiano <marcoscssno@gmail.com>
2023-03-31 19:59:26 -0300

    feat: get user by id route

--- src/routes.ts
+++ src/routes.ts
@@ -1,6 +1,7 @@
 import { Router } from "express";
 import { createUserController } from "./core/useCase/createUser";
 import { getAllUsersController } from "./core/useCase/getAllUsers";
+import { getUserByIdController } from "./core/useCase/getUserById";
 
 const router = Router()
 
@@ -10,6 +11,10 @@ router.post('/users', (request, response) => {
 
 router.get('/users', async (request, response) => {
     return getAllUsersController.handle(request, response);
+});
+
+router.get('/user/:id', async (request, response) => {
+    return getUserByIdController.handle(request, response);
 });
 
 export { router }
