f6ad5cf55eeef71164c9672b61e0830c209ce33c
Marcos Cassiano <marcoscssno@gmail.com>
2023-04-01 22:31:25 -0300

    test: ensure it updates user properties

--- src/core/repository/user/implementation/InMemoryUserRepository.test.ts
+++ src/core/repository/user/implementation/InMemoryUserRepository.test.ts
@@ -21,4 +21,24 @@ describe('In Memory User Repository', async () => {
         const inMemoryUser = inMemoryUsers[0];
         expect(inMemoryUser).toEqual(user);
     })
+    it('should update user properties', async () => {
+        const newUser = new User({
+            name: 'Marcos',
+            email: 'marcos@example.com',
+            encryptedPassword: 'somePassword'
+        });
+        const sut = new InMemoryUserRepository();
+        await sut.save(newUser);
+        const userId = newUser.getId();
+        const updatedUser = new User({
+            name: 'Otto',
+            email: 'otto@example.com',
+            encryptedPassword: 'otherPassword'
+        });
+        await sut.update(userId, updatedUser);
+        const user = await sut.getUserById(userId);
+        expect(user?.getName()).toEqual(updatedUser.getName());
+        expect(user?.getEmail()).toEqual(updatedUser.getEmail());
+        expect(user?.getEncryptedPassword()).toEqual(updatedUser.getEncryptedPassword());
+    })
 })
