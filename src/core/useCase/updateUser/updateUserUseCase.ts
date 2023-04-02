8c9f1d90492bb898430d698555801bb7cb190823
Marcos Cassiano <marcoscssno@gmail.com>
2023-04-02 00:33:50 -0300

    feat: update user use case

---
+++ src/core/useCase/updateUser/updateUserUseCase.ts
@@ -0,0 +1,41 @@
+import { User } from "../../entity/user/User";
+import { PasswordEncryptorInterface } from "../../lib/passwordEncryptor/PasswordEncryptorInterface";
+import { UserRepositoryInterface } from "../../repository/user/UserRepositoryInterface";
+import { CreateUserRequestDTO } from "../createUser/createUserRequestDTO";
+
+class UpdateUserUseCase {
+    constructor(
+        private userRepository: UserRepositoryInterface,
+        private passwordEncryptor: PasswordEncryptorInterface
+        ) {}
+    async execute(id: string, user: CreateUserRequestDTO): Promise<void> {
+        if(!this.userRepository) {
+            throw new Error('User repository was not specified');
+        }
+        if(!id) {
+            throw new Error('Id was not specified');
+        }
+        if(typeof(id) !== 'string') {
+            throw new Error('Invalid id type');
+        }
+        if(!user) {
+            throw new Error('User was not specified');
+        }
+        if(typeof(user) !== 'object') {
+            throw new Error('Invalid user type');
+        }
+        const { name, email, password } = user;
+        const encryptedPassword = await this.passwordEncryptor.execute(password);
+        const userProps = {
+            name,
+            email,
+            encryptedPassword
+        }
+        const updatedUser = new User(userProps);
+        this.userRepository.update(id, updatedUser);
+    }
+}
+
+export {
+    UpdateUserUseCase
+}
