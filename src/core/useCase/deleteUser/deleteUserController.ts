3b3e101ac001d88d4b33f308e0004352bf4fafd2
Marcos Cassiano <marcoscssno@gmail.com>
2023-04-06 09:28:21 -0300

    feat: delete user controller

---
+++ src/core/useCase/deleteUser/deleteUserController.ts
@@ -0,0 +1,25 @@
+import { Request, Response } from 'express';
+import { DeleteUserUseCase } from "./deleteUserUseCase";
+
+class DeleteUserController {
+    constructor(
+        private deleteUserUseCase: DeleteUserUseCase
+    ) { }
+    async handle(request: Request, response: Response): Promise<Response> {
+        const { id } = request.params;
+
+        try {
+            await this.deleteUserUseCase.execute(id);
+            return response.status(204).send();
+        }
+        catch (error) {
+            return response.status(400).json({
+                message: error.message || 'Unexpected error'
+            });
+        }
+    }
+}
+
+export {
+    DeleteUserController
+}
